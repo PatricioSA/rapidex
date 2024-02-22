3992753151d75d6b906e630a2eb12d9eeee49e48
Patrício Samuel <patricioanjosjf@gmail.com>
2024-02-22 19:31:52 -0300

    organizar pastas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/layout/Header";
-import HomeMenu from "./components/layout/HomeMenu";
-import MainSection from "./components/layout/MainSection";
+import Header from "../components/layout/Header";
+import HomeMenu from "../components/layout/HomeMenu";
+import MainSection from "../components/layout/MainSection";
 
 export default function Home() {
   return (
