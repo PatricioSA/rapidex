4fae35aac3f8e8c82225f343972bfbd3a8adb060
Patrício Samuel <patricioanjosjf@gmail.com>
2024-02-28 12:47:27 -0300

    images

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,65 +1,65 @@
 import { Product } from "./types";
 
-export const burgers: Product[] = [
+export const burguers: Product[] = [
     {
         id: 1,
-        image: '',
+        image: '/img/x-bacon.png',
         name: 'X-Bacon',
         description: 'pão, maionese, carne, queijo, alface, tomate, bacon',
         price: 15
     },
     {
         id: 2,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Salada',
         description: 'pão, maionese, carne, queijo, alface, tomate',
         price: 12
     },
     {
         id: 3,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Egg',
         description: 'pão, maionese, carne, queijo, ovo',
         price: 13
     },
     {
         id: 4,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Tudo',
         description: 'pão, maionese, carne, queijo, alface, tomate, bacon, ovo',
         price: 18
     },
     {
         id: 5,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Frango',
         description: 'pão, maionese, frango desfiado, queijo, alface, tomate',
         price: 14
     },
     {
         id: 6,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Burger',
         description: 'pão, maionese, carne, queijo',
         price: 10
     },
     {
         id: 8,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Calabresa',
         description: 'pão, maionese, linguiça calabresa, queijo, alface, tomate',
         price: 13
     },
     {
         id: 11,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Gourmet',
         description: 'pão artesanal, maionese trufada, carne angus, queijo brie, cebola caramelizada, rúcula',
         price: 25
     },
     {
         id: 15,
-        image: '...',
+        image: '/img/x-bacon.png',
         name: 'X-Cheese',
         description: 'pão, maionese, muito queijo',
         price: 11
@@ -69,7 +69,7 @@ export const burgers: Product[] = [
 export const drinks = [
     {
         id: 16,
-        image: '...',
+        image: '/',
         name: 'Coca-Cola',
         description: 'Refrigerante de cola',
         price: 5,
@@ -77,7 +77,7 @@ export const drinks = [
 
     {
         id: 17,
-        image: '...',
+        image: '/',
         name: 'Guaraná Antarctica',
         description: 'Refrigerante de guaraná',
         price: 4,
@@ -85,7 +85,7 @@ export const drinks = [
 
     {
         id: 18,
-        image: '...',
+        image: '/',
         name: 'Fanta Laranja',
         description: 'Refrigerante de laranja',
         price: 4,
@@ -93,7 +93,7 @@ export const drinks = [
 
     {
         id: 19,
-        image: '...',
+        image: '/',
         name: 'Sprite',
         description: 'Refrigerante de limão-limão',
         price: 4,
@@ -101,7 +101,7 @@ export const drinks = [
 
     {
         id: 20,
-        image: '...',
+        image: '/',
         name: 'Suco de Laranja',
         description: 'Suco natural de laranja',
         price: 6,
@@ -109,7 +109,7 @@ export const drinks = [
 
     {
         id: 21,
-        image: '...',
+        image: '/',
         name: 'Suco de Uva',
         description: 'Suco natural de uva',
         price: 6,
@@ -117,7 +117,7 @@ export const drinks = [
 
     {
         id: 22,
-        image: '...',
+        image: '/',
         name: 'Água Mineral',
         description: 'Água pura sem gás',
         price: 3,
@@ -125,7 +125,7 @@ export const drinks = [
 
     {
         id: 23,
-        image: '...',
+        image: '/',
         name: 'Água com Gás',
         description: 'Água carbonatada',
         price: 3,
