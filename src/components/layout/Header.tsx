55b5b0b2e4cf3a1e3cf541627fca263d0aaab5e3
Patrício Samuel <patricioanjosjf@gmail.com>
2024-02-28 22:22:49 -0300

    adicionar classe active para os links

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,10 +9,16 @@ export default function Header() {
     const [mobileNavOpen, setMobileNavOpen] = useState(false)
     const { cartProducts } = useContext(CartContext)
 
+    const [activeLink, setActiveLink] = useState("/");
+
+    const handleLinkClick = (link: string) => {
+        setActiveLink(link);
+    };
+
     return (
         <header>
             <div className="flex justify-between items-center md:hidden">
-                <Link className="text-primary font-semibold text-2xl" href=''>
+                <Link className="text-primary font-semibold text-2xl" href='/'>
                     RAPIDEX
                 </Link>
 
@@ -20,7 +26,11 @@ export default function Header() {
                     <button onClick={() => setMobileNavOpen(prevState => !prevState)}>
                         <TfiMenu size={25} />
                     </button>
-                    <Link href={'/cart'} className="relative">
+                    <Link
+                        href={'/cart'}
+                        className={`relative ${activeLink === "/cart" ? "active" : ""}`}
+                        onClick={() => handleLinkClick('/cart')}
+                    >
                         <HiOutlineShoppingCart size={25} />
                         <span className="absolute -top-2 -right-4 bg-primary
                             text-white text-xs py-1 px-2 rounded-full leading-3">
@@ -34,22 +44,61 @@ export default function Header() {
                 <div className="md:hidden p-4 bg-gray-200 rounded-lg mt-2 text-center">
                     <nav className="flex flex-col items-center gap-2 text-gray-500 
                 font-semibold">
-                        <Link href='/'>Home</Link>
-                        <Link href='/menu'>Menu</Link>
-                        <Link href=''>Contato</Link>
+                        <Link
+                            href='/'
+                            className={activeLink === "/" ? "active" : ""}
+                            onClick={() => handleLinkClick('/')}
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            href='/menu'
+                            className={activeLink === "/menu" ? "active" : ""}
+                            onClick={() => handleLinkClick('/menu')}
+                        >
+                            Menu
+                        </Link>
+                        <Link
+                            href='/#contact'
+                            className={activeLink === "/#contact" ? "active" : ""}
+                            onClick={() => handleLinkClick('/#contact')}
+                        >
+                            Contato
+                        </Link>
                     </nav>
                 </div>
             )}
 
             <div className="hidden md:flex items-center justify-between">
-                <Link className="text-primary font-semibold text-2xl" href=''>
+                <Link className="text-primary font-semibold text-2xl" href='/'>
                     RAPIDEX
                 </Link>
                 <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-                    <Link href='/'>Home</Link>
-                    <Link href='/menu'>Menu</Link>
-                    <Link href=''>Contato</Link>
-                    <Link href={'/cart'} className="relative">
+                    <Link
+                        href='/'
+                        className={activeLink === "/" ? "active" : ""}
+                        onClick={() => handleLinkClick('/')}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        href='/menu'
+                        className={activeLink === "/menu" ? "active" : ""}
+                        onClick={() => handleLinkClick('/menu')}
+                    >
+                        Menu</Link>
+                    <Link
+                        href='/#contact'
+                        className={activeLink === "/#contact" ? "active" : ""}
+                        onClick={() => handleLinkClick('/#contact')}
+                    >
+                        Contato
+                    </Link>
+                    <Link
+                        href={'/cart'}
+                        className={`relative ${activeLink === "/cart" ? "active" : ""}`}
+                        onClick={() => handleLinkClick('/cart')}
+                    >
                         <HiOutlineShoppingCart size={25} />
                         <span className="absolute -top-2 -right-4 bg-primary
                             text-white text-xs py-1 px-2 rounded-full leading-3">
