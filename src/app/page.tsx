8bcade5077b3154d9365701bbbbc9e7fd9c90e1c
Patrício Samuel <patricioanjosjf@gmail.com>
2024-02-28 21:27:41 -0300

    criar seção de contato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/layout/Header";
+import Titles from "@/components/layout/Titles";
 import HomeMenu from "../components/layout/HomeMenu";
 import MainSection from "../components/layout/MainSection";
 
@@ -7,6 +7,14 @@ export default function Home() {
     <>
       <MainSection />
       <HomeMenu/>
+      <section className="mt-16 text-center">
+        <Titles mainTitle="Contato" subTitle="Fale conosco"/>
+
+        <a className="text-4xl underline text-blue-400"
+        href="tel:+5532912345678" id="contact">
+          +55 32 91234 5678
+        </a>
+      </section>
     </>
   );
 }
