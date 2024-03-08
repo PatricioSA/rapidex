16b0972bfbaf0ec703e1975b2e85e6e6a5922d50
Patrício Samuel <patricioanjosjf@gmail.com>
2024-03-08 10:38:55 -0300

    feat: Adicionadas informações ao footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,23 @@
 export default function Footer() {
     return (
-        <footer className="text-center text-gray-500 border p-8 mt-16">
-            &copy; 2024 Todos os direitos reservados
+        <footer className="text-gray-500 border px-8 py-4 mt-16">
+
+            <section className="grid md:grid-cols-2 gap-4 mb-8 md:ml-16">
+                <section>
+                    <h2 className="font-semibold">Horário de Funcionamento</h2>
+                    <p>
+                        Segunda a Sexta: <time>18:00</time> - <time>00:00</time> <br />
+                        Sábado: <time>18:00</time> - <time>02:00</time>
+                    </p>
+                </section>
+
+                <section>
+                    <h2 className="font-semibold">Endereço</h2>
+                    <p>Rua Compre Aqui, 145 - Centro, Juiz de Fora - MG</p>
+                </section>
+            </section>
+
+            <div className="text-center">&copy; 2024 Todos os direitos reservados</div>
         </footer>
     )
 }
